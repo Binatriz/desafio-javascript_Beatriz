64c4bcc5ed467880874c6d9101045ab05f621e27
Beatriz Cerqueira <beatrizcerqueira008@gmail.com>
2025-10-20 09:17:22 -0300

    Carousel adicionado

--- js/carousel.js
+++ js/carousel.js
@@ -9,24 +9,33 @@ let carouselArr = [];
 //class Carousel
 class Carousel {
 
-    
-      
-    static Start(arr){
-        if(arr){
+    constructor(img, texto, link) {
+        this.img = img;
+        this.texto = texto;
+        this.link = link;
+    }
 
-            if(arr.length > 0){
-                Carousel._sequence = 0;
-                Carousel._size = arr.length;
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
-            }
-            
-        } else {
-            throw "Method Start need a Array Variable.";
+    static Start(arr) {
+
+        if (arr.length > 0) {
+            Carousel._arr = arr;
+            Carousel._sequence = 0;
+            Carousel._size = arr.length;
+            Carousel.Next(); //start
+            Carousel._interval = setInterval(function () { Carousel.Next(); }, 3000);
         }
     }
 
-    static Next(){
-        
+    static Next() {
+        const carousel = document.getElementById('carousel');
+        const carouselTitle = document.getElementById('carousel-title');
+        const atual = Carousel._arr[Carousel._sequence];
+
+        carousel.innerHTML = `<img src="./img/${atual.img}" alt=""></img>`;
+        carouselTitle.innerHTML = `<a href="${atual.link}">${atual.texto}</a>`;
+        Carousel._sequence++;
+        if (Carousel._sequence >= Carousel._size) {
+            Carousel._sequence = 0;
+        }
     }
 };
